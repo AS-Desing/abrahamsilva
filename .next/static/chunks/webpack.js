b5fcf3de36cc943181020d78c795afc5a6960896
Urobukis <adsm120@gmail.com>
2024-10-08 17:20:41 -0400

    segundo commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec2d7fd98bd73f7b"; }
+/******/ 		__webpack_require__.h = function() { return "153a5cf3c8fdbf4a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
