53e0d2c4fe700fd0513eb6e3ffebabcc9fbd6830
Urobukis <adsm120@gmail.com>
2024-10-08 20:00:21 -0400

    para lanzar

---
+++ .next/server/chunks/793.js
@@ -0,0 +1,217 @@
+"use strict";
+exports.id = 793;
+exports.ids = [793];
+exports.modules = {
+
+/***/ 503:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ layaout)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: ./components/footer.js
+
+const Footer = ()=>/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx("footer", {
+            className: "",
+            children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: "flex w-full flex-wrap content-center justify-center items-justify mx-auto md:columns-3 sm:columns-1 place-items-center gap-8",
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("ul", {
+                        children: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                            href: "https://www.linkedin.com/in/abraham-silva-011093203/",
+                            children: /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+                                class: "w-6 h-6 text-blue-500 fill-current",
+                                xmlns: "http://www.w3.org/2000/svg",
+                                viewBox: "0 0 448 512",
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                                    d: "M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                                })
+                            })
+                        })
+                    }),
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: "md:colum-1 md:row-2 sm:flex-wrap justify-center items-center content-center ",
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+                                class: "w-6 h-6 text-green-400 fill-current mx-auto",
+                                xmlns: "http://www.w3.org/2000/svg",
+                                viewBox: "0 0 448 512",
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                                    d: "M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
+                                })
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx("h4", {
+                                className: "font-sans font-semibold text-xl",
+                                children: "+584128397679"
+                            })
+                        ]
+                    }),
+                    " ",
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: "colum-1 row-2 justify-center items-center content-center",
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+                                class: "w-6 h-6 text-green-400 fill-current mx-auto",
+                                xmlns: "http://www.w3.org/2000/svg",
+                                version: "1.0",
+                                viewBox: "0 0 239.000000 170.000000",
+                                preserveAspectRatio: "xMidYMid meet",
+                                children: /*#__PURE__*/ jsx_runtime_.jsx("g", {
+                                    transform: "translate(0.000000,170.000000) scale(0.100000,-0.100000)",
+                                    fill: "#000000",
+                                    stroke: "none",
+                                    children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                                        d: "M0 906 l0 -793 23 -34 c12 -18 38 -44 56 -56 l34 -23 1082 0 1082 0 34 23 c18 12 44 38 56 56 l23 34 0 793 0 794 -1195 0 -1195 0 0 -794z m2320 746 c0 -15 -1060 -867 -1089 -876 -65 -19 -77 -11 -626 434 -292 237 -531 435 -533 440 -2 7 392 10 1122 10 619 0 1126 -4 1126 -8z m-1595 -591 c198 -161 377 -301 397 -312 47 -24 99 -24 146 0 32 17 202 153 909 728 l153 124 0 -737 0 -736 -34 -34 -34 -34 -1067 0 -1067 0 -34 34 -34 34 0 736 0 737 153 -124 c83 -68 314 -255 512 -416z"
+                                    })
+                                })
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx("h4", {
+                                className: "font-sans font-semibold text-lg",
+                                children: "urobukis@gmail.com"
+                            })
+                        ]
+                    })
+                ]
+            })
+        })
+    });
+/* harmony default export */ const footer = (Footer);
+
+// EXTERNAL MODULE: ./pages/index.js
+var pages = __webpack_require__(678);
+;// CONCATENATED MODULE: ./components/images.js
+
+
+const imagen = [
+    "1.jpg",
+    ,
+    "black-week-2.jpg",
+    "cerveza1.jpg",
+    "cerveza-2.jpg",
+    "cerveza-3.jpg",
+    "cerveza-4.jpg",
+    "dore-halloween.jpg",
+    "propuesta-2.jpg",
+    "propuesta-4.jpg",
+    "propuesta-3.jpg",
+    "propuesta-5.jpg",
+    "propuesta-6.jpg",
+    "propuesta-7.jpg",
+    "propuesta-post1.jpg",
+    "propuesta-post2.jpg",
+    "propuesta-post3.jpg",
+    "dore-cancer.jpg",
+    "tres-relojes.jpg"
+];
+const imagen2 = [
+    "cafe.jpg",
+    "001.jpg",
+    "donuts.jpg"
+];
+const imagen3 = [
+    "gmg-eventos",
+    "tbr.png",
+    "caypops.png"
+];
+const imagenes_prestar = imagen.map((e, Index)=>/*#__PURE__*/ jsx_runtime_.jsx("img", {
+        src: imagen[Index]
+    }));
+const imagenes_prestar2 = imagen2.map((e, Index)=>/*#__PURE__*/ jsx_runtime_.jsx("img", {
+        src: imagen2[Index]
+    }));
+const imagenes_prestar3 = imagen3.map((e, Index)=>/*#__PURE__*/ jsx_runtime_.jsx("img", {
+        src: imagen3[Index]
+    }));
+const Images = ()=>/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                className: "text-center text-3xl",
+                children: "Redes Sociales"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "grid sm:grid-cols-1 md:grid-cols-3 gap-3 md:mx-5 sm:ml-0",
+                children: imagenes_prestar
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                className: "text-center text-3xl",
+                children: "moockup"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: "grid md:grid-cols-3 gap-3 mx-5",
+                children: imagenes_prestar2
+            })
+        ]
+    });
+/* harmony default export */ const components_images = (Images);
+
+;// CONCATENATED MODULE: ./components/sobremi.js
+
+const SobreMi = ()=>/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+            className: "grid md:grid-cols-2 md:ml-6 bg-about md:mt-8 md:mb-8 ungap",
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: "md:ml-[12rem]",
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("img", {
+                        className: "md:w-80",
+                        src: "foto-perfil.jpeg"
+                    })
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: "text-center items-center content-center txt",
+                    children: /*#__PURE__*/ jsx_runtime_.jsx("p", {
+                        className: "text-center sm:text-center content-center smtxt lg:text-xl xl:text-2xl text-justify mx-6",
+                        children: "Siempre me he sentido apasionado por el dise\xf1o y la programaci\xf3n, pero no fue sino hasta el inicio de la pandemia que me decid\xed a dedicarme a mi pasi\xf3n.En la actualidad trabajo de forma Frelance tanto en la creaci\xf3n de publicidad para redes sociales como la programaci\xf3n de sitios web enfoc\xe1ndome especialmente en el uso de react. Siempre estoy buscando aprender m\xe1s y crecer en este maravilloso mundo creativo para volver la visi\xf3n de mis clientes en una realidad."
+                    })
+                })
+            ]
+        })
+    });
+/* harmony default export */ const sobremi = (SobreMi);
+
+;// CONCATENATED MODULE: ./components/layaout.js
+
+
+
+
+const Layout = ({ children  })=>/*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            children,
+            /*#__PURE__*/ jsx_runtime_.jsx(sobremi, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(components_images, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(footer, {})
+        ]
+    });
+/* harmony default export */ const layaout = (Layout);
+
+
+/***/ }),
+
+/***/ 678:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_layaout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(503);
+
+
+const Index = ()=>/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_layaout__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {})
+    });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Index);
+
+
+/***/ })
+
+};
+;
