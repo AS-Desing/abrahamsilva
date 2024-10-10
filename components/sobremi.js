f90c2f8a990911b27b57c976c8bd6b35de3dd191
Urobukis <33214019+urobukis@users.noreply.github.com>
2024-10-10 10:53:03 -0400

    Update sobremi.js

--- components/sobremi.js
+++ components/sobremi.js
@@ -4,7 +4,7 @@ const SobreMi=()=>(
             <div className="md:ml-[12rem]">
                 <img className="md:w-80" src="foto-perfil.jpeg"/>
             </div>
-            <div className="text-center items-center content-center text-justify lg:text-2xl xl:text-3xl txt">
+            <div className="text-center items-center content-center text-justify lg:text-2xl xl:text-3xl">
             <p className="text-center sm:text-center content-center smtxt  mx-6">Siempre me he sentido apasionado por el diseño y la programación, pero no fue 
             sino hasta el inicio de la pandemia que me decidí a dedicarme a mi pasión.En la actualidad trabajo de forma Frelance tanto en la creación de publicidad 
             para redes sociales como la programación de sitios web enfocándome especialmente 
